0823815ad5491ff20cc3961fa920e12d44369ec1
Brandon Parsons <parsons.brandon@gmail.com>
2014-06-07 22:32:33 -0600

    UserMailer additional methods

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,50 @@
 class UserMailer < ActionMailer::Base
 
+  def etf_purchase_instructions(user_id, rebalance_amount)
+    user                              = User.find user_id
+    @rebalance_info, @etf_info_lookup = rebalance_data(user, rebalance_amount)
+    @amount                           = rebalance_amount
+
+    @new_funds_url = ""
+    @dashboard_url = ""
+
+    mail(to: user.email, subject: 'ETF Purchasing Instructions') do |format|
+      format.text
+      format.html
+    end
+  end
+
+
+  def portfolio_out_of_balance(user_id)
+    user                              = User.find user_id
+    @rebalance_info, @etf_info_lookup = rebalance_data(user, 0)
+
+    @new_funds_url = ""
+    @dashboard_url = ""
+    @edit_account_url = ""
+
+    mail(to: user.email, subject: 'Portfolio out of Balance') do |format|
+      format.text
+      format.html
+    end
+  end
+
+
+  def min_rebalance_spacing(user_id)
+    user = User.find user_id
+
+    @tracked_portfolio_url = ""
+    @dashboard_url = ""
+    @new_funds_url = ""
+    @edit_account_url = ""
+
+    mail(to: user.email, subject: 'Portfolio Check-in') do |format|
+      format.text
+      format.html
+    end
+  end
+
+
   def reset_password_instructions(email, set_password_request: false)
     user    = RegularUser.find_from_all_users_with_email(email)
     subject = set_password_request ? 'Set Password Request' : 'Password Reset Instructions'
@@ -15,6 +60,7 @@ class UserMailer < ActionMailer::Base
       end
     end
   end
+
 
   def confirm_email_instructions(email: nil, user_id: nil)
     raise "Email required." unless email.present?
@@ -33,6 +79,15 @@ class UserMailer < ActionMailer::Base
         format.html
       end
     end
+  end
+
+
+  private
+
+  def rebalance_data(user, rebalance_amount)
+    rebalance_info  = user.portfolio.rebalance(rebalance_amount)
+    etf_info_lookup = Etf.info_lookup_table(rebalance_info.keys.concat(user.portfolio.current_shares.keys).uniq)
+    return rebalance_info, etf_info_lookup
   end
 
 end
