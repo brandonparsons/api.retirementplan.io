4933cea491623e5d71476bfa83b5b8cc40a0b0d8
Brandon Parsons <parsons.brandon@gmail.com>
2014-06-11 21:53:21 -0600

    Send whether or not the user has accepted terms

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :image, :has_password, :confirmed,
-    :completed_questionnaire
+    :completed_questionnaire, :accepted_terms
 
   has_many  :authentications
   has_one   :questionnaire
@@ -21,6 +21,10 @@ class UserSerializer < ActiveModel::Serializer
 
   def completed_questionnaire
     object.has_completed_questionnaire?
+  end
+
+  def accepted_terms
+    object.has_accepted_terms?
   end
 
 end
