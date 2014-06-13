17e5259b761b93458746945efdd4d8ee7ecc6bed
Brandon Parsons <parsons.brandon@gmail.com>
2014-06-13 07:23:07 -0600

    Serialize data about user's progress to tracked portfolio

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :image, :has_password, :confirmed,
-    :completed_questionnaire, :accepted_terms
+  attributes :id, :name, :email, :image, :has_password, :confirmed, :accepted_terms,
+    :has_completed_questionnaire, :has_selected_portfolio, :has_completed_simulation,
+    :has_tracked_portfolio
 
   has_many  :authentications
   has_one   :questionnaire
@@ -19,12 +20,24 @@ class UserSerializer < ActiveModel::Serializer
     object.confirmed?
   end
 
-  def completed_questionnaire
+  def accepted_terms
+    object.has_accepted_terms?
+  end
+
+  def has_completed_questionnaire
     object.has_completed_questionnaire?
   end
 
-  def accepted_terms
-    object.has_accepted_terms?
+  def has_selected_portfolio
+    object.has_selected_portfolio?
+  end
+
+  def has_completed_simulation
+    object.has_completed_simulation?
+  end
+
+  def has_tracked_portfolio
+    object.has_setup_tracked_portfolio?
   end
 
 end
