e016b03dac6e4f7bfe76350899543143317f6f28
Brandon Parsons <parsons.brandon@gmail.com>
2014-07-10 21:28:42 -0600

    Add scopes for first/last on authentication/security. Apparently first/last won't work properly when using UUIDS's

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,6 +1,9 @@
 class Authentication < ActiveRecord::Base
   # Contains OAuth data from third parties: Facebook, Google, etc.
 
+  scope :first, -> { order("created_at").first }
+  scope :last, -> { order("created_at DESC").first }
+
   belongs_to :user, touch: true
 
   validates :uid, presence: true, uniqueness: { scope: :provider }
--- app/models/security.rb
+++ app/models/security.rb
@@ -1,5 +1,9 @@
 class Security < ActiveRecord::Base
 
+  scope :first, -> { order("created_at").first }
+  scope :last, -> { order("created_at DESC").first }
+
+
   ################
   # ASSOCIATIONS #
   ################
