3fe7c2166bc28493c647f39059e15143317ea149
Brandon Parsons <parsons.brandon@gmail.com>
2014-07-12 22:50:10 -0600

    Remove first/last scopes

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,9 +1,6 @@
 class Authentication < ActiveRecord::Base
   # Contains OAuth data from third parties: Facebook, Google, etc.
 
-  scope :first, -> { order("created_at").first }
-  scope :last, -> { order("created_at DESC").first }
-
   belongs_to :user, touch: true
 
   validates :uid, presence: true, uniqueness: { scope: :provider }
--- app/models/security.rb
+++ app/models/security.rb
@@ -1,8 +1,5 @@
 class Security < ActiveRecord::Base
 
-  scope :first, -> { order("created_at").first }
-  scope :last, -> { order("created_at DESC").first }
-
 
   ################
   # ASSOCIATIONS #
