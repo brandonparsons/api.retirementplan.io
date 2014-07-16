dc45a6d1e95b1fc3d27635be35ff30e5bc66d603
Brandon Parsons <parsons.brandon@gmail.com>
2014-07-16 00:34:39 -0600

    Only floatmapper portfolio in serializer if the data is present

--- app/serializers/portfolio_serializer.rb
+++ app/serializers/portfolio_serializer.rb
@@ -2,11 +2,11 @@ class PortfolioSerializer < ActiveModel::Serializer
   attributes :id, :weights, :current_shares, :selected_etfs, :tracking
 
   def weights
-    FloatMapper.call(object.weights)
+    FloatMapper.call(object.weights) if object.weights.present?
   end
 
   def current_shares
-    FloatMapper.call(object.current_shares)
+    FloatMapper.call(object.current_shares) if object.current_shares.present?
   end
 
 end
