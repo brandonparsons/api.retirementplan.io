a632c7ca3611a5705bbae057b9d0a98dd26381c8
Brandon Parsons <parsons.brandon@gmail.com>
2014-07-16 00:14:55 -0600

    Put js_error route in teh correct locatoin

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -16,23 +16,4 @@ class MiscController < ApplicationController
     render text: '', content_type: 'text/plain'
   end
 
-  def js_error
-    stack   = params[:stack]
-    json    = params[:responseJSON]
-    message = params[:message]
-
-    if defined?(Rollbar)
-      Rollbar.report_message("JS Error Reported!", "warning", {
-        stack: stack,
-        json: json,
-        message: message
-      })
-    else
-      logger.warn "JS Error Report:"
-      logger.warn stack
-      logger.warn json
-      logger.warn message
-    end
-  end
-
 end
--- app/controllers/v1/misc_controller.rb
+++ app/controllers/v1/misc_controller.rb
@@ -6,6 +6,27 @@ module V1
       render json: {simulations: $redis.get($SIMULATION_COUNT_KEY) }
     end
 
+    def js_error
+      stack   = params[:stack]
+      json    = params[:responseJSON]
+      message = params[:message]
+
+      if defined?(Rollbar)
+        Rollbar.report_message("JS Error Reported!", "warning", {
+          stack: stack,
+          json: json,
+          message: message
+        })
+      else
+        logger.warn "JS Error Report:"
+        logger.warn stack
+        logger.warn json
+        logger.warn message
+      end
+
+      render json: {success: true, message: 'Error posted.'}
+    end
+
   end
 
 end
