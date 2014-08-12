3e4c36b8b973ffe0376dad97961d2cc7d691d8d1
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-12 06:18:10 -0600

    Log client ID

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -4,6 +4,7 @@ module V1
     before_action :authenticate_user!, except: [:create]
 
     def create
+      logger.warn "GA Client ID: #{params[:ga_client_id]}"
       @user = RegularUser.new(user_create_params)
       if @user.save
         @user.sign_in!
