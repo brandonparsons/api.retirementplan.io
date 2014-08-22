ef231bfb26fa549e5d8ac0d290d3e662675a400e
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-22 05:13:37 -0600

    Rescue in_balance call when no portfolio set up

--- app/serializers/admin_portfolio_serializer.rb
+++ app/serializers/admin_portfolio_serializer.rb
@@ -2,11 +2,13 @@ class PortfolioSerializer < ActiveModel::Serializer
   attributes :market_value, :in_balance
 
   def market_value
+    # Report no value if they haven't set up yet.....
     object.current_shares.present? ? object.send(:current_market_value) : 0.0
   end
 
   def in_balance
-    object.out_of_balance?(0.05)
+    # Report in balance if they haven't set up yet....
+    object.current_shares.present? ? object.out_of_balance?(0.05) : true
   end
 
 end
