a1da2a74e8c73b856d1dc0c98d3b96dd4076f5b6
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-21 21:55:03 -0600

    Need admin control, not just current_user

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,5 +1,12 @@
 module Admin
   class AdminController < ApplicationController
     before_action :authenticate_user!
+    before_action :must_be_admin
+
+    protected
+
+    def must_be_admin
+      return access_denied unless current_user.admin?
+    end
   end
 end
