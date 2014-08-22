bdc2b9736e71eb603c791fcb71b9e5a991c36187
Brandon Parsons <parsons.brandon@gmail.com>
2014-08-22 05:10:11 -0600

    Rescue market value call when no current shares

--- app/serializers/admin_portfolio_serializer.rb
+++ app/serializers/admin_portfolio_serializer.rb
@@ -2,7 +2,7 @@ class PortfolioSerializer < ActiveModel::Serializer
   attributes :market_value, :in_balance
 
   def market_value
-    object.send(:current_market_value)
+    object.current_shares.present? ? object.send(:current_market_value) : 0.0
   end
 
   def in_balance
